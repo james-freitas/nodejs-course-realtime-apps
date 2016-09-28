8c73317f6dd331c35cf13dc9416fdd100bb478a7
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2016-09-28 08:10:42 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Node realtime apps course
---
+++ first-app/node-module/leitura_async.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+var leituraAsync = function(arquivo){
+    console.log("Fazendo leitura assíncrona");
+    var inicio = new Date().getTime();
+    fs.readFile(arquivo);
+    var fim = new Date().getTime();
+    console.log("Bloqueio assincrono: "+ (fim - inicio) + "ms");
+};
+
+module.exports = leituraAsync;
